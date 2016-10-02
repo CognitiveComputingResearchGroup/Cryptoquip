f16472940d29212dd5405b684d8db2ef7443ee31
Stephen Strain <sfstrain@memphis.edu>
2016-10-01 23:37:20 -0500

    cq_nodes.py added.

--- src/cqlida/scripts/cq_nodes.py
+++ src/cqlida/scripts/cq_nodes.py
@@ -6,13 +6,24 @@ import string
 import rospy
 from std_msgs.msg import String
 
-import cq_classes
 import cq_utilities
 
+puzzle = cq_utilities.puzzlify("Here is the message.")
+trans_dict = puzzle.get_trans_dict()
+puzzle.unset("".join(trans_dict.keys()))
 
-
-pub = rospy.Publisher('puzzle_state', String)
-rospy.init_node('environment')
-rate = rospy.Rate(10) # 10 Hz
-while not rospy.is_shutdown():
+def cq_environment():
+    pub = rospy.Publisher('puzzle_state', String, queue_size=10)
+    rospy.init_node('environment')
+    rate = rospy.Rate(10) # 10 Hz
+    while not rospy.is_shutdown():
+        pub_string = str(puzzle)
+        #rospy.loginfo(pub_string)
+        pub.publish(pub_string)
+        rate.sleep()
     
+if __name__ == '__main__':
+    try:
+        cq_environment()
+    except rospy.ROSInterruptException:
+        pass
